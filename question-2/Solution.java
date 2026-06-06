d6a2a9d5c610bb8a7c0367c13d113393b416242b
nabiii4 <nabeehaahmad04@gmail.com>
2026-06-06 17:08:44 -0400

    Add Bottle class with updateAmount method
    Implement Bottle class with capacity and amount management.

--- question-2/Solution.java
+++ question-2/Solution.java
@@ -1,1 +1,26 @@
 
+public class Bottle{
+  private double capacity;
+  private double amoun;
+    public Bottle(double capacity){
+      this.capacity = capacity;
+      amoun = capacity;
+    }
+    public double updateAmount(double used){
+      double limit = 0.25 * capacity;
+      //int placeholder = capacity;
+      amoun -= used;
+      if((amoun) < limit){
+        amoun= capacity;
+    }
+    return amoun;
+      
+      //else{
+        //placeholder - used;
+        //return placeholder;
+      //}
+  }
+}
+      
+      
+  
