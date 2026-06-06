767300856b9c23aa25dc1668e7768f38f990c61c
nabiii4 <nabeehaahmad04@gmail.com>
2026-06-06 15:02:47 -0400

    Implement username availability check in Account constructor

--- question-1/Solution.java
+++ question-1/Solution.java
@@ -1,1 +1,32 @@
 
+public Account(requestedName){
+  if(isAvailable(requestedName) == true){
+    username = requestedName;
+  }else{
+    String practiceOne = requestedName;
+    for(int i=1; isAvailable(practiceOne) == false; i++){
+      practiceOne = requestedName;
+      practiceOne+= i;
+    }
+    username = practiceOne;
+  }
+}
+
+
+
+
+public String getShortenedName(){
+    String newName = "";
+    for (int i = 0; i <username.length(); i++){
+        if (username.charAt(i) == '-'){
+            newName = newName.substring(0, newName.length() - 1);
+        }
+        else {
+            newName += username.charAt(i);
+        }
+    }
+    return newName;
+}
+
+
+
