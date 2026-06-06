f8ef96e4f00ad46fb2dbe4260843918262b7174c
nabiii4 <nabeehaahmad04@gmail.com>
2026-06-06 17:14:38 -0400

    Implement getPointsForRow method
    Add method to calculate points for a specific row based on color.

--- question-4/Solution.java
+++ question-4/Solution.java
@@ -1,1 +1,18 @@
 
+public int getPointsForRow(int targetRow){
+    int total = 0;
+    String firstColor = board[targetRow][0].getColor();
+    boolean sameColor= true;
+    for (int c=0; c<board[targetRow].length; c++){
+        total += board[targetRow][col].getPoints();
+
+        if (!board[targetRow][c].getColor().equals(firstColor)){
+            sameColor = false;
+        }
+    }
+
+    if(sameColor){
+        return total * 2;
+    }
+    return total;
+}
